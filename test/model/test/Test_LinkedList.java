349c643b32066500db4fa9d504042dde278ff76e
unknown <tomaslangebaek@gmail.com>
2019-02-15 21:15:57 -0500

    test

--- test/model/test/Test_LinkedList.java
+++ test/model/test/Test_LinkedList.java
@@ -3,35 +3,34 @@ package model.test;
 import junit.framework.TestCase;
 import model.data_structures.LinkedList;
 import model.data_structures.Nodo;
+import model.data_structures.Stack;
+import model.moving_violations.VOMovingViolations;
 
 public class Test_LinkedList<T> extends TestCase {
 
 	
     private LinkedList<T> listaEncadenada;
     
-    private Nodo<T> nodo1;
-    
-    private Nodo<T> nodo2;
-    
-    private Nodo<T> nodo3;
 
-  
     private void escenario1( )
     {
-        listaEncadenada = new LinkedList<>();
+        
     }
     /**
      * Agrega algunos nodos a la lista para realizar las pruebas
      */
     private void escenario2()
     {
-//    	escenario1();
-//    	T elemento = "String";
-//    	listaEncadenada.agregarFinal(nodo1);
-//    	nodo2 = new Nodo<T>(null);
-//    	listaEncadenada.agregarFinal(nodo2);
-//    	nodo3 = new Nodo<T>(null);
-//    	listaEncadenada.agregarFinal(nodo3);
+    	escenario1();
+    	VOMovingViolations violacion1 = new VOMovingViolations(1, 10, "calle1", 111, 101, 101010, 010101, "tipo1", 1000, 100, 0, 0, "Yes", "date1", 0, 0, "v1");
+    	VOMovingViolations violacion2 = new VOMovingViolations(2, 20, "calle2", 222, 202, 202020, 020202, "tipo2", 2000, 200, 0, 0, "Yes", "date2", 0, 0, "v1");
+    	VOMovingViolations violacion3 = new VOMovingViolations(3, 30, "calle3", 333, 303, 303030, 030303, "tipo3", 3000, 300, 0, 0, "No", "date3", 0, 0, "v1");
+    	
+    	listaEncadenada.agregarFinal(violacion1);
+    	
+    	listaEncadenada.agregarFinal(violacion2);
+    	
+    	listaEncadenada.agregarFinal(violacion3);
     }
     
 
