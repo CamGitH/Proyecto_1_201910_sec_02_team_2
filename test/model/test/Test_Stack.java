7fae461594af5692fe1b2810417fc46b634cdcd1
unknown <camilo.orgc@gmail.com>
2019-02-17 23:45:18 -0500

    mira

--- test/model/test/Test_Stack.java
+++ test/model/test/Test_Stack.java
@@ -1,5 +1,35 @@
 package model.test;
 
-public class Test_Stack {
+import junit.framework.TestCase;
 
+public class Test_Stack  extends TestCase{
+
+		private model.data_structures.Queue<String> s;
+
+
+		public void setUp() {
+			s = new model.data_structures.Stack<String>();
+
+			System.out.println("Codigo de iniciacion");
+			System.out.println("prueba");
+		}
+
+
+		public void test() {
+
+			s.enqueue("n1");
+			s.enqueue("n2");
+			s.enqueue("n3");
+			s.enqueue("n4");
+			s.enqueue("n");
+			if(s.size()!=5){
+				fail("Error clase");
+			}
+			assertEquals("No funciona el añadido del queue","n",s.getUltimo().darElemento());
+			assertEquals("La extraccion de info no funciona","n1",s.dequeue());
+			assertEquals("No se elimino el primer elemento despues de consultarlos","n2",s.getPrimero().darElemento());
+
+			
+		
+	}
 }
