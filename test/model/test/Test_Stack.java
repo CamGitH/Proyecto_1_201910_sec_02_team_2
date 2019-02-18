64268d43f839cdcfeec9397b1a12c6e3dee6580b
unknown <tomaslangebaek@gmail.com>
2019-02-17 23:53:05 -0500

    gggg

--- test/model/test/Test_Stack.java
+++ test/model/test/Test_Stack.java
@@ -1,14 +1,15 @@
 package model.test;
 
 import junit.framework.TestCase;
+import model.data_structures.Stack;
 
 public class Test_Stack  extends TestCase{
 
-		private model.data_structures.Queue<String> s;
+		private Stack<String> s;
 
 
 		public void setUp() {
-			s = new model.data_structures.Stack<String>();
+			s = new Stack<String>();
 
 			System.out.println("Codigo de iniciacion");
 			System.out.println("prueba");
@@ -17,18 +18,18 @@ public class Test_Stack  extends TestCase{
 
 		public void test() {
 
-			s.enqueue("n1");
-			s.enqueue("n2");
-			s.enqueue("n3");
-			s.enqueue("n4");
-			s.enqueue("n");
+			s.push("n1");
+			s.push("n2");
+			s.push("n3");
+			s.push("n4");
+			s.push("n");
 			if(s.size()!=5){
 				fail("Error clase");
 			}
-			assertEquals("No funciona el añadido del queue","n",s.getUltimo().darElemento());
-			assertEquals("La extraccion de info no funciona","n1",s.dequeue());
-			assertEquals("No se elimino el primer elemento despues de consultarlos","n2",s.getPrimero().darElemento());
-
+			assertEquals("No funciona pop","n1",s.pop());
+			assertEquals("No funciona pop","n2",s.pop());
+			assertEquals("No funciona pop","n3",s.pop());
+			
 			
 		
 	}
